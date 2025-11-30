81cb0b0cc6fb26211e63f1de406b752f6fe943d6
Butterblock233 <Voltage15312@outlook.com>
2025-11-30 15:14:45 +0000

    nvim: update nvim depedences(Language servers and foratters)

--- common/neovim.nix
+++ common/neovim.nix
@@ -7,14 +7,23 @@
 {
   environment.systemPackages = with pkgs; [
     # Language Servers
-    gopls
-    basedpyright
-    rust-analyzer
-    lua-language-server
-    nil # Language Server for `Nix`
 
+    # golang
+    gopls
+    # Python
+    basedpyright
+    # rust
+    rust-analyzer
+    # lua
+    lua-language-server
+    stylua
+    # Language Server for `Nix`
+    nil
+    # C++
     xmake
     cmake
+    clang-tools
+
   ];
   # 将默认编辑器设置为 neovim
   environment.variables = {
