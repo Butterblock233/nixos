644cbe10cbf33b2f3adcec141244a8e167de46bc
Himalian <Voltage15312@outlook.com>
2026-06-14 17:44:39 +0000

    move remote.nix to wsl specific config

--- wsl/init.nix
+++ wsl/init.nix
@@ -9,7 +9,7 @@
     ../common/neovim.nix
     ../common/packages.nix
     ../common/env.nix
-    ../common/remote.nix
+    ./remote.nix
     ./networking.nix
     ../common/languages.nix
     ../common/vituralization.nix
