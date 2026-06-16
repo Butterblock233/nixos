d2eb00943a5835aff0fa2d46e0cf114dceb4704c
Himalian <Voltage15312@outlook.com>
2026-06-16 07:49:34 +0000

    chore: update registry

--- common/registy.nix
+++ common/registy.nix
@@ -4,5 +4,6 @@
     pkgs.flake = inputs.nixpkgs;
     nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
     pkgs-unstable.flake = inputs.nixpkgs-unstable;
+    unstable.flake = inputs.nixpkgs-unstable;
   };
 }
